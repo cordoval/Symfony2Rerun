1810ae7b7cfe7019f0fc20118ef4a16aba18b7d0
Luis Cordova <cordoval@gmail.com>
2013-05-11 15:13:27 -0500

    first commit

---
+++ chess/tests/clean-writeable-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m chess -p clean-writeable [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "clean-writeable"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
