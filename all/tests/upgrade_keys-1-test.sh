f47f5b0e6fae689ec13410275eb9350eb7f5d9eb
Luis Cordova <cordoval@gmail.com>
2013-07-02 08:27:32 -0500

    added upgrade key command

---
+++ all/tests/upgrade_keys-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m all -p upgrade_keys [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "upgrade_keys"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
